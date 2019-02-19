50bb125b3a51f3503805ab60ec34cfb15a54fc80
Jonas Japing <jonasjaping@gmail.com>
2019-02-19 17:05:06 +1100

    the tasks manager in Rails :-)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,50 @@
 class TasksController < ApplicationController
+
+before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    # @task is drawn into here via before_action and set_task
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    # ^ Model#create is Model#new + instance#save
+    redirect_to tasks_path
+    # redirects back to index for better UX
+  end
+
+  def edit
+    # @task is drawn into here via before_action and set_task
+  end
+
+  def update
+    # @task is drawn into here via before_action and set_task
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    # @task is drawn into here via before_action and set_task
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  resources :tasks
+
+
+
+  # CRUD
+
+  # READ - All instances
+  # get 'tasks', to: 'tasks#index'
+
+  # # CREAT
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+
+  # # READ - One instance
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # UPDATE
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # DELETE
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :delete
 end
+
+
